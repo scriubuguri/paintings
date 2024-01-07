53f5e47a4e40722d35177370ad02a34016aa5eb0
scriubuguri <scriubuguri@tragdate.ninja>
2024-01-07 20:34:49 +0200

    functional app

--- my_paintings/my_app/views.py
+++ my_paintings/my_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def my_home_page(request):
+    return HttpResponse("<h1>Hellooo human being!</h1>")
